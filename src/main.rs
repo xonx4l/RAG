562bff49cce9f0de9cf646f56aa2e2268c79393a
xonx <119700621+xonx4l@users.noreply.github.com>
2024-03-30 16:27:28 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
+mod vector;
+mod contents;
+
+use  contents::File;
+use  qdrant_client::qdrant::QdrantClient,
+
+struct AppState {
+    files: Vec<File>,
+    vector_db:VectorDB,
+}
+
+async fn hello_world() -> &'static str {
+    "Hello world!"
+}
 
