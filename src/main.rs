c8deee34214676cfaf6fa7b6e3c8b0d3d4b7d4d7
xonx <119700621+xonx4l@users.noreply.github.com>
2024-04-01 23:45:48 +0530

    Update runtime

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,19 @@ async fn hello_world() -> &'static str {
     "Hello world!"
 }
 
+#[shuttle_runtime::main]
+async fn axum(
+    #[shuttle_secrets::Secrets] secrets: shuttle_secrets::SecretStore,
+    #[shuttle_qdrant::Qdrant(
+        cloud_url = "{secrets.QDRANT_URL}",
+        api_key = "{secrets.QDRANT_TOKEN}"
+    )]
+    qdrant_client: QdrantClient,
+) -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+      .route("/", get(hello_world));
+
+    Ok(router.into())  
+}
+
+
