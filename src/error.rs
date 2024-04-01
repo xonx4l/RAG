544cb468875242fcd978e6d51107439e3bb68cbd
xonx <119700621+xonx4l@users.noreply.github.com>
2024-04-01 23:50:59 +0530

    Update error.rs

---
+++ src/error.rs
@@ -0,0 +1,8 @@
+#[derive (Debug)]
+pub struct SetupError(pub &'static str);
+impl std::error::Error for SetupError {}
+impl std::fmt::Display for SetupError {
+    fn fmt(&self, f:&mut std::fmt::Formatter<'_>) -> std::fnt::Result {
+        write!(f, "Error: {}", self.0)
+    }
+}
