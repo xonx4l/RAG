70dc4e373c23501caa36d225a9fb18b079915046
xonx <119700621+xonx4l@users.noreply.github.com>
2024-04-03 16:32:38 +0530

    setup embedding errors

--- src/error.rs
+++ src/error.rs
@@ -6,3 +6,28 @@ impl std::fmt::Display for SetupError {
         write!(f, "Error: {}", self.0)
     }
 }
+#[derive(Debug)]
+pub struct EmbeddingError;
+
+impl std::error::Error for EmbeddingError {}
+impl Display for EmbeddingError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Embedding error")
+    }
+}
+
+impl From<anyhow::Error> for EmbeddingError {
+    fn from(_: anyhow::Error) -> Self {
+        Self{}
+    }
+}
+
+#[derive(Debug)]
+pub struct NotAvailableError;
+
+impl std::error::Error for NotAvailableError {}
+impl std::fmt::Display for NotAvailableError {
+    fn fmt(&self, f:&mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "File' not available' error")
+    }
+}
