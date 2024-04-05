b0bc4e93f6a90f0a23f284e12c75a9dfff961098
xonx <119700621+xonx4l@users.noreply.github.com>
2024-04-05 23:12:20 +0530

    create embeddings

--- src/open_ai.rs
+++ src/open_ai.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use openai::embeddings::{Embedding, Embeddings};
+use crate::errors::EmbeddingError;
 
 pub fn setup(secrets: &SecretStore) -> Result<()> {
 
@@ -7,4 +9,17 @@ pub fn setup(secrets: &SecretStore) -> Result<()> {
        .ok_or(SetupError("OPENAI key not available "))?;
     openai::set_key(openai_key);
     Ok(())
+}
+
+pub async fn embed_file(file: &File) -> Result<Embeddings> {
+    let sentence_as_str: Vec<&str> = file.sentences.iter().map(|s| s.as_str()).collect();
+    Embeddings::create("text-embedding-ada-002", sentence_as_str, "shuttle")
+         .await
+         .map_err(|_| EmbeddingError {}.into())
+}
+
+pub async fn embed_sentence(prompt: &str) -> Result<Embeddings> {
+    Embedding::create("text-embedding-ada-002", prompt, "shuttle")
+        .await
+        .map_err(|_| EmbeddingError {}.into)
 }
