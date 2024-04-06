1d1cf83f4643267cd4340fea21a3b59724979811
xonx <119700621+xonx4l@users.noreply.github.com>
2024-04-07 00:07:25 +0530

    Update vector.rs

--- src/vector.rs
+++ src/vector.rs
@@ -1,5 +1,5 @@
 use qdrant_client::qdrant::QdrantClient;
-
+use crate::errors::EmbeddingError;
 pub struct VectorDB {
     client: QdrantClient,
     id:u64,
@@ -8,5 +8,23 @@ pub struct VectorDB {
 impl VectorDB {
     pub fn new (client: QdrantClient)-> Self {
         Self { client, id: 0}
+
+    pub async fn upsert_embedding(&mut self ,embedding:Embedding, file: &File) -> Result<()> {
+        let payload: Payload = json!({
+            "id": file.path.clone(),
+        })
+        .try_into()
+        .map_err(|_| EmbeddingError {})?;
+
+        println!("Embedded: {}", file.path);
+
+        let vec: Vec<f32> = embedding.vec.iter().map(|&x| x as f32). collect();
+
+        let points = vec![PointStruct::new(self.id, vec, payload)];
+        self.client.upsert_points(COLLECTION, None, points, None).await?;
+        self.id +=1;
+
+        Ok(())
+    }
     }
 }
